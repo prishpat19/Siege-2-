26ee3d6e3354d663b35a4390bb6108c8fdab919d
prishpat19 <72462997+prishpat19@users.noreply.github.com>
2022-01-27 18:40:26 -0500

    Add files via upload

---
+++ tower-siege--2-main/Block.js
@@ -0,0 +1,48 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.image = loadImage("block.png");
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var angle = this.body.angle;
+
+      console.log(this.body.speed);
+      var pos= this.body.position;
+    if(this.body.speed < 10){
+      //super.display();
+      imageMode(CENTER); 
+      image(this.image,pos.x,pos.y,this.width, this.height);
+     
+    }
+
+    else{
+
+      World.remove(world, this.body);
+      push();
+      this.Visibility = this.Visibility - 5;
+      tint(255, this.Visibility);
+      translate(this.body.position.x, this.body.position.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+      
+      //image(this.image, 0, 0, this.width, this.height);
+      pop();
+
+
+   
+    }
+
+
+    
+   
+    
+    }
+}
